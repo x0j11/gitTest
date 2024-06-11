6b6448c28b98988bb1ff8bd42281e6adf33eed36
Haamid683 <haamidmotara@gmail.com>
2024-06-11 03:10:58 +0100

    Second commit. Added function1

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,3 +1,9 @@
+def function1(x: int):
+    if (x > 3):
+        print("X is more than 3")
+    else:
+        print("X is less than or equal to 3")
+
 print("Hello World")
 x = 1
 y = 2
@@ -5,5 +11,10 @@ y = 2
 z = x + y
 
 print(z)
+function1(z)
+
+
 print(z*z)
+function1(z*z)
+
 print("Yeet")
